a2cccf3e2fddb25e195aef60fd9627fd921c00f9
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-03-16 17:42:33 +0200

    Add option to switch to F degrees and back to C

--- src/displayWeatherData.js
+++ src/displayWeatherData.js
@@ -5,8 +5,9 @@ const searchInput = document.querySelector('input[type="text"]');
 const locationTitle = document.querySelector(".location-title");
 const currentTemperature = document.querySelector(".current-temp");
 const weatherDescription = document.querySelector(".weather-description");
-const feelsLike = document.querySelector(".feels-like");
+const feelsLikeTemp = document.querySelector(".feels-like-temp");
 const errorField = document.querySelector("#error-field");
+const measureSwitcher = document.querySelector(".measure-switcher");
 
 export default async function displayWeatherData(location) {
   try {
@@ -15,7 +16,7 @@ export default async function displayWeatherData(location) {
     locationTitle.textContent = weatherData.fullLocation;
     currentTemperature.textContent = `${weatherData.currentTemperature}°`;
     weatherDescription.textContent = weatherData.weatherDescription;
-    feelsLike.textContent = `Feels like ${weatherData.temperatureFeelsLike}°`;
+    feelsLikeTemp.textContent = `${weatherData.temperatureFeelsLike}°`;
   } catch (err) {
     errorField.textContent =
       err.message === "Location not found."
@@ -24,8 +25,47 @@ export default async function displayWeatherData(location) {
   }
 }
 
+function temperatureConverter(system, temp) {
+  let convertedTemp;
+  if (system === "C°") {
+    convertedTemp = Number(temp) * (9 / 5) + 32;
+  } else if (system === "F°") {
+    convertedTemp = (Number(temp) - 32) * (5 / 9);
+  }
+  return convertedTemp.toFixed(0);
+}
+
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const location = searchInput.value;
   displayWeatherData(location);
+});
+
+measureSwitcher.addEventListener("click", () => {
+  const currentSystem = measureSwitcher.textContent;
+  if (currentSystem.includes("C")) {
+    const newCurrentTemperature = temperatureConverter(
+      currentSystem,
+      currentTemperature.textContent.slice(0, -1)
+    );
+    const newFeelsLikeTemperature = temperatureConverter(
+      currentSystem,
+      feelsLikeTemp.textContent.slice(0, -1)
+    );
+    currentTemperature.textContent = `${newCurrentTemperature}°`;
+    feelsLikeTemp.textContent = `${newFeelsLikeTemperature}°`;
+    measureSwitcher.textContent = "F°";
+  } else {
+    const newCurrentTemperature = temperatureConverter(
+      currentSystem,
+      currentTemperature.textContent.slice(0, -1)
+    );
+    const newFeelsLikeTemperature = temperatureConverter(
+      currentSystem,
+      feelsLikeTemp.textContent.slice(0, -1)
+    );
+    currentTemperature.textContent = `${newCurrentTemperature}°`;
+    feelsLikeTemp.textContent = `${newFeelsLikeTemperature}°`;
+    measureSwitcher.textContent = "C°";
+  }
 });
--- src/fetchWeatherData.js
+++ src/fetchWeatherData.js
@@ -1,6 +1,13 @@
+const loadingContainer = document.querySelector(".loading-container");
+
 export default async function getData(location) {
+  loadingContainer.classList.add("loading");
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f3283dc6489429f85319c9f78c856248&units=metric`;
   const response = await fetch(url, { mode: "cors" });
   const weatherData = await response.json();
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  loadingContainer.classList.remove("loading");
+
   return weatherData;
 }
